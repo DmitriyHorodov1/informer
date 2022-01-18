56665116c0bdb076414e9021ac9e0bcae21d8044
DmitriyHorodov1 <10vova.demiraki@gmail.com>
2022-01-18 14:10:59 +0200

    16.01.22 first topic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstTopicComponent } from './first-topic/first-topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTopicComponent
   ],
   imports: [
     BrowserModule
