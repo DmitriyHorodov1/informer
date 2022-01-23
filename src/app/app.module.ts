bb2256d894219fc13bed7a63fd85b87318cd0720
DmitriyHorodov1 <gorodovdmitro@gmail.com>
2022-01-23 15:07:42 +0200

    23.01.22 firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AddPostModule } from './add-post';
 import { AppComponent } from './app.component';
 import { FirstTopicComponent } from './first-topic/first-topic.component';
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import{AngularFireModule} from "@angular/fire/compat";
+import { FormsModule } from '@angular/forms';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB4qRf06JfIlsa9cnz7ILcD1j0ZugMZgf8",
+  authDomain: "informer-one.firebaseapp.com",
+  projectId: "informer-one",
+  storageBucket: "informer-one.appspot.com",
+  messagingSenderId: "682542257347",
+  appId: "1:682542257347:web:126d59bf6c7477fb1b7e76",
+  measurementId: "G-G97TLC1FMX"
+};
 
 
 @NgModule({
@@ -15,7 +27,10 @@ import { FirstTopicComponent } from './first-topic/first-topic.component';
   ],
   imports: [
     BrowserModule,
-    AddPostModule
+    AddPostModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
 
 
   ],
--- src/app/first-topic/first-topic.component.ts
+++ src/app/first-topic/first-topic.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AddPostService } from '../add-post';
-
+import {Observable} from "rxjs";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {UUID} from 'uuid-generator-ts';
 @Component({
   selector: 'app-first-topic',
   templateUrl: './first-topic.component.html',
@@ -11,8 +13,12 @@ export class FirstTopicComponent implements OnInit {
   bodyText: string ="" ;
   title = '';
   content = [];
+  ErrorInput: String ="";
+  posts: Observable<any[]> | undefined;
 
-  constructor( private modalService: AddPostService ) { }
+  constructor( private modalService: AddPostService,  public db:AngularFirestore ) {
+    this.posts = db.collection('posts').valueChanges();
+  }
 
   ngOnInit() {
     this.bodyText = 'This text can be updated in modal 1';
@@ -26,4 +32,26 @@ export class FirstTopicComponent implements OnInit {
   closeModal(id: string) {
     this.modalService.close(id);
   }
+  async addPost(){
+
+    if(this.title !='' && this.content !=[]){
+      this.ErrorInput=""
+      const uuid = new UUID();
+      var id : string = uuid.getDashFreeUUID();
+
+
+      this.db.collection('posts').doc(id).set({
+        title : this.title,
+        content:this.content,
+
+        Id :  id
+      });
+    }
+    else{
+      console.log("Error Input")
+      this.ErrorInput = "Please Input title and content"
+
+    }
+  }
 }
+
